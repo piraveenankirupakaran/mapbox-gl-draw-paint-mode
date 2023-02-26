e09b3e74b0285292a3845efd3dbc346fb76d3157
piraveenankirupakaran <piraveenankirupakaran@gmail.com>
2023-02-26 11:22:17 +0000

    Added Common_selectors

--- demo/frontend/src/App.js
+++ demo/frontend/src/App.js
@@ -9,31 +9,12 @@ import PaintMode from "mapbox-gl-draw-paint-mode";
 import "maplibre-gl/dist/maplibre-gl.css";
 import { extendDrawBar } from "./utils/extendDrawBar";
 
-import MapboxWorker from "worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker";
-mapboxgl.workerClass = MapboxWorker; // Wire up loaded worker to be used instead of the default
-
 const App = () => {
   const mapContainer = useRef(null);
   const mapRef = useRef(null);
   const mapboxDrawRef = useRef(null);
-  const [markerLabel, setMarkerLabel] = useState("Marker Label");
 
   const [isAddingMarker, setIsAddingMarker] = useState(false);
-  const [lineColor, setLineColor] = useState("#f4424b");
-
-  const handleMarkerButtonClick = () => {
-    setIsAddingMarker(!isAddingMarker);
-  };
-
-  const handleMapClick = (event) => {
-    const lngLat = event.lngLat;
-    const marker = new maplibregl.Marker({ draggable: true })
-      .setLngLat(lngLat)
-      .setPopup(null)
-      .addTo(mapRef.current);
-
-    setIsAddingMarker(false);
-  };
 
   useEffect(() => {
     if (!mapContainer) {
@@ -58,28 +39,6 @@ const App = () => {
         ...MapboxDraw.modes,
         draw_paint_mode: PaintMode,
       },
-      // styles: [
-      //   // Customize the line style to use the lineColor state variable
-      //   {
-      //     id: "gl-draw-line",
-      //     type: "line",
-      //     filter: [
-      //       "all",
-      //       ["==", "$type", "LineString"],
-      //       ["!=", "mode", "static"],
-      //     ],
-      //     layout: {
-      //       "line-cap": "round",
-      //       "line-join": "round",
-      //     },
-      //     paint: {
-      //       "line-color": lineColor,
-      //       "line-dasharray": [0.2, 2],
-      //       "line-width": 4,
-      //       "line-opacity": 0.7,
-      //     },
-      //   },
-      // ],
     });
     const drawPaintBtn = {
       on: "click",
@@ -118,22 +77,8 @@ const App = () => {
     mapRef.current.addControl(drawBar);
   }, []);
 
-  useEffect(() => {
-    if (!mapContainer || !isAddingMarker) {
-      return;
-    }
-
-    mapRef.current.on("click", handleMapClick);
-    return () => {
-      mapRef.current.off("click", handleMapClick);
-    };
-  }, [isAddingMarker]);
-
   return (
     <div className="map-wrap">
-      <button onClick={handleMarkerButtonClick}>
-        {isAddingMarker ? "Stop Adding Markers" : "Add Marker"}
-      </button>
       <button
         className="custom_btn"
         onClick={() => {
